d0a802fb73517b20c1f5c077d3033b0b34b576db
NguyenVanHung1707 <hungnguyen.170704@gmail.com>
2025-06-06 05:54:00 +0700

    updateupdate

---
+++ test/client/src/Routes/index.js
@@ -0,0 +1,64 @@
+import ClassRoom from "../Apps/Class";
+import Layout from "../Apps/Layout";
+import ClassHand from "../Apps/Class/class-hand"
+import ClassCreateEhust from "../Apps/Class/classCreateEhust";
+import Login from "../Apps/login";
+import Register from "../Apps/register";
+import Dashboard from "../Apps/dashboard";
+import Vehicle from "../Apps/vehicle";
+import Resident from "../Apps/resident";
+import Invoices from "../Apps/invoices";
+import Statistics from "../Apps/statistics";
+import FeeAndFund from "../Apps/feeAndFund";
+import PrivateRoutes from "../components/privateRoutes";
+import Apartment from "../Apps/apartment";
+export const Routes = [
+    {
+        path: "/login",
+        element: <Login />
+    },
+    {
+        path: "/register",
+        element: <Register />
+    },
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            {
+                element: <PrivateRoutes />,
+                children: [
+                    {
+                        path: '/tong-quan',
+                        element: <Dashboard />
+                    },
+                    {
+                        path: '/can-ho',
+                        element: <Apartment />
+                    },
+                    {
+                        path: '/xe-co',
+                        element: <Vehicle />
+                    },
+                    {
+                        path: '/cu-dan',
+                        element: <Resident />
+                    },
+                    {
+                        path: '/hoa-don',
+                        element: <Invoices />
+                    },
+                    {
+                        path: '/thong-ke',
+                        element: <Statistics />
+                    },
+                    {
+                        path: '/phi-va-quy',
+                        element: <FeeAndFund />
+                    },
+                    
+                ]
+            }
+        ]
+    }
+]
